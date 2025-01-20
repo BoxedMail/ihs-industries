130402d04157a20dbc1e87fc5fe2b942bd5c7c4e
BoxedMail <ubayades222@gmail.com>
2025-01-20 21:07:10 +1100

    reinstalled packages - Double industries problem

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,14 +5,19 @@ import OurIndustries from '../OurIndustries';
 import ToServices from '../ToServices';
 import Logo from '../Logo';
 import MiniAbout from '../MiniAbout';
+import { Helmet } from 'react-helmet';
 
 function Home () {
     return (
         <>
+
+            <Helmet>
+                <title>Home - IHS Engineering Co</title>
+            </Helmet>
+            
             <HeroSection />
             <Logo/>
             <MiniAbout />
-            <OurIndustries />
             <ToServices />
             <OurIndustries />
         
