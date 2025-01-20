fb97430bab220d02e703196d95770ea9fa5405e5
BoxedMail <ubayades222@gmail.com>
2025-01-21 03:16:11 +1100

    Added pages for services and about

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Contact from './components/pages/Contact';
+import About from './components/pages/About';
+import Services from './components/pages/Services';
 import Footer from './components/Footer';
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" exact element = {<Home/>}></Route>
+          <Route path="/about" exact element = {<About/>}></Route>
+          <Route path="/services" exact element = {<Services/>}></Route>
           <Route path="/contact" exact element = {<Contact/>}></Route>
         </Routes>
         <Footer />
